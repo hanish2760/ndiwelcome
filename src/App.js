f554b9a5607a7658c5eb7bc146e62ee644313fc7
Hanish Kumar Langaru <hlangaru@ezesoft.com>
2024-09-16 18:16:20 +0530

    Browser Tab Update
    Homepage updated from Github pagers URL to Domain URL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import mandala from './art.jpeg'; // Make sure to use the correct path
+
+// src/App.js
+
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
+      <h1 className="intro-heading">NDI is LIVE</h1>
+      <img src={mandala} alt="NDI LIVE Mandala" className="rotating-mandala" />
     </div>
   );
 }
-
 export default App;
